d97124f2ea0f047db7d20b402e3f351a3e417b5a
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-09-27 12:45:05 +0200

    assets folder added and picture linked

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,15 +6,15 @@ function App() {
 
   return (
     <>
+      <h1>Felipe Moreira Salas, M.S.</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a target="_blank">
+          <img src='../assets/picture.JPG' className="logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src='../assets/picture.JPG' className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
