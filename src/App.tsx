76089156462bbc21cabf5782bd2aa2eb265f107e
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-09-27 15:55:37 +0200

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,31 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Felipe Moreira Salas, M.S.</h1>
-      <div>
-        <a target="_blank">
-          <img src='../assets/picture.JPG' className="logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src='../assets/picture.JPG' className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <h1>Felipe Moreira Salas, M.S.</h1>
+          <div className="text-3xl font-bold underline">
+              Hello world!
+          </div>
+          <div>
+              <a target="_blank">
+                  <img src='../assets/picture.JPG' className="logo"/>
+              </a>
+              <a href="https://react.dev" target="_blank">
+                  <img src='../assets/picture.JPG' className="logo react" alt="React logo"/>
+              </a>
+          </div>
+          <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+              </button>
+              <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+          </div>
+          <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+          </p>
+      </>
   )
 }
 
