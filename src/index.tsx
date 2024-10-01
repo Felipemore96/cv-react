e32acc19fdd98ad596c595d27d8a634fb05557e0
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-01 16:24:43 -0600

    Organized components and declared Main component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './react-components/App.tsx'
+import { Main } from './react-components/Main.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
 )
--- src/react-components/Education.tsx
+++
@@ -1,6 +0,0 @@
-export function Education() {
-
-    return (
-        <></>
-    )
-}
