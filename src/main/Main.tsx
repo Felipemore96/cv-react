d359aca49bdcf53a54c61dffd46b4898f69a4eab
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-05 16:36:35 -0600

    Add main

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -2,10 +2,9 @@ import { useState } from "react";
 import "../styles/App.module.css";
 export function Main() {
   return (
-    <>
-      <a target="_blank">
-        <img src="../../assets/photos/picture.JPG" className="logo" />
-      </a>
-    </>
+    <section>
+      <img src="../../assets/photos/picture.JPG" className="logo" />
+      <div></div>
+    </section>
   );
 }
