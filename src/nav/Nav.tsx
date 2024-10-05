454f18e53ebe554436a18e1b7a5b7f3b7d8a41dd
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-04 19:41:25 -0600

    nav styles

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -1,21 +1,26 @@
 import { useLocation } from "react-router-dom";
+import styles from "../styles/Nav.module.css";
 export function Nav() {
   return (
-    <nav>
-      <a href="/">Felipe Moreira Salas, M.S.</a>
-      <div>
-        <li>
-          <a href="#About">About</a>
-        </li>
-        <li>
-          <a href="#Experience">Experience</a>
-        </li>
-        <li>
-          <a href="#Projects">Projects</a>
-        </li>
-        <li>
-          <a href="#Contact">Contact</a>
-        </li>
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Felipe Moreira Salas, M.S.
+      </a>
+      <div className={styles.menu}>
+        <ul className={styles.menuItems}>
+          <li>
+            <a href="#About">About</a>
+          </li>
+          <li>
+            <a href="#Experience">Experience</a>
+          </li>
+          <li>
+            <a href="#Projects">Projects</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
