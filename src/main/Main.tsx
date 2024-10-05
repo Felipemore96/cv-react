8dfcc0c687f06927e9b702555d7bdbaf184bf66b
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-05 17:55:57 -0600

    Add Main content

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
-import "../styles/App.module.css";
+import styles from "../styles/Main.module.css";
 export function Main() {
   return (
-    <section>
-      <img src="../../assets/photos/picture.JPG" className="logo" />
-      <div></div>
+    <section className={styles.container}>
+      <img src="../../assets/photos/picture.JPG" className={styles.photo} />
+      <div className={styles.content}>
+        <p className={styles.description}>Hello, I'm</p>
+        <h1 className={styles.title}>Felipe Moreira</h1>
+        <p className={styles.description}>BIM Software Developer</p>
+        <div className={styles.content}>
+          <a className={styles.BtnLarge}>Download CV</a>
+          <a className={styles.BtnLarge}>Contact Me</a>
+        </div>
+        <div className={styles.content}>
+          <a className={styles.BtnSmall}>LnI</a>
+          <a className={styles.BtnSmall}>GtH</a>
+        </div>
+      </div>
     </section>
   );
 }
