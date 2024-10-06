fb004d12d53e2a8915fbe1fded395b94231081a6
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-05 18:51:46 -0600

    Add Main style classes

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -3,18 +3,18 @@ import styles from "../styles/Main.module.css";
 export function Main() {
   return (
     <section className={styles.container}>
-      <img src="../../assets/photos/picture.JPG" className={styles.photo} />
+      <img src="../../assets/photos/picture.png" className={styles.photo} />
       <div className={styles.content}>
         <p className={styles.description}>Hello, I'm</p>
         <h1 className={styles.title}>Felipe Moreira</h1>
         <p className={styles.description}>BIM Software Developer</p>
-        <div className={styles.content}>
-          <a className={styles.BtnLarge}>Download CV</a>
-          <a className={styles.BtnLarge}>Contact Me</a>
+        <div className={styles.firstBtnLine}>
+          <a className={styles.btnLarge}>Download CV</a>
+          <a className={styles.btnLarge}>Contact Me</a>
         </div>
-        <div className={styles.content}>
-          <a className={styles.BtnSmall}>LnI</a>
-          <a className={styles.BtnSmall}>GtH</a>
+        <div className={styles.secondBtnLine}>
+          <a className={styles.btnSmall}>LnI</a>
+          <a className={styles.btnSmall}>GtH</a>
         </div>
       </div>
     </section>
