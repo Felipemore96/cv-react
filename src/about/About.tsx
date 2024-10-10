99f440a52b43ba50c9de1f91e7704d62d3f16b6f
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-09 18:44:25 -0600

    Add about fields

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,7 +1,25 @@
+import styles from "../styles/About.module.css";
+
 export function About() {
   return (
-    <section id="about">
-      <h1>HELLO</h1>
+    <section className={styles.container} id="about">
+      <p className={styles.description}>Get To Know More</p>
+      <h1 className={styles.title}>About Me</h1>
+      <div>
+        <ul>
+          <li>Master 1</li>
+          <li>Master 2</li>
+          <li>Licentiate</li>
+        </ul>
+        <div>
+          <p>Location</p>
+          <div>Description</div>
+          <ul>
+            <li>1 year BIM S.D.</li>
+            <li>2 years Construction Manager</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
