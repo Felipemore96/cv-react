bfd6f87164dcc7d40da36df159f1f7c674633aa2
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-10 18:00:20 -0600

    Initial styles to about section

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -5,11 +5,11 @@ export function About() {
     <section className={styles.container} id="about">
       <p className={styles.description}>Get To Know More</p>
       <h1 className={styles.title}>About Me</h1>
-      <div>
-        <ul>
-          <li>Master 1</li>
-          <li>Master 2</li>
-          <li>Licentiate</li>
+      <div className={styles.content}>
+        <ul className="flex flex-col">
+          <li className={styles.master}>Master 1</li>
+          <li className={styles.master}>Master 2</li>
+          <li className={styles.master}>Licentiate</li>
         </ul>
         <div>
           <p>Location</p>
