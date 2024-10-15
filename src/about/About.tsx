44b3ecc67abac99482d3983c981fe98bd72111bc
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-14 21:48:39 -0600

    styles to master courses and info

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -6,11 +6,24 @@ export function About() {
       <p className={styles.description}>Get To Know More</p>
       <h1 className={styles.title}>About Me</h1>
       <div className={styles.content}>
-        <ul className="flex flex-col">
-          <li className={styles.master}>Master 1</li>
-          <li className={styles.master}>Master 2</li>
-          <li className={styles.master}>Licentiate</li>
-        </ul>
+        <div className={styles.mastersList}>
+          <div className={styles.master}>
+            <p className={styles.masterText}>Master BIM Software Developer</p>
+            <p className={styles.uniText}>That Open Company</p>
+          </div>
+          <div className={styles.master}>
+            <p className={styles.masterText}>BIM A+ European Master</p>
+            <p className={styles.uniText}>
+              University of Ljubljana & University of Minho
+            </p>
+          </div>
+          <div className={styles.master}>
+            <p className={styles.masterText}>
+              Bachelor's Degree in Civil Engineering
+            </p>
+            <p className={styles.uniText}>Universidad de Costa Rica</p>
+          </div>
+        </div>
         <div>
           <p>Location</p>
           <div>Description</div>
