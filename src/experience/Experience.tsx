babedae52be8b373ee0a1efea72c9bb02eb99f2f
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-15 22:00:07 -0600

    Experience section created

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -1,9 +1,11 @@
+import styles from "../styles/Experience.module.css";
+
 export function Experience() {
   return (
-    <>
+    <section className={styles.container} id="experience">
       <label>
         <span className="material-icons-round">person</span>Contact
       </label>
-    </>
+    </section>
   );
 }
