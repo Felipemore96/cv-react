52b6d52f902da40d486fadb388d0da9826ec322b
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-17 21:55:35 -0600

    initial jobs added to experience

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -1,11 +1,21 @@
 import styles from "../styles/Experience.module.css";
+import skills from "../../src/data/skills.json";
+import jobs from "../../src/data/jobs.json";
 
 export function Experience() {
   return (
     <section className={styles.container} id="experience">
-      <label>
-        <span className="material-icons-round">person</span>Contact
-      </label>
+      <h1 className={styles.title}>Experience</h1>
+
+      <ul>
+        {jobs.map((jobsItem, id) => {
+          return (
+            <li key={id}>
+              <h3>{jobsItem.role}</h3>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
