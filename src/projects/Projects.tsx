4065efc9528de72996058a639414a9b53c19d36b
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-22 22:01:33 -0600

    Complete info in two small project boxes and style fixes

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -24,7 +24,84 @@ export function Projects() {
             src={getImageUrl("icons/about/bimmodel.png")}
             className={styles.projectImage}
           />
-          <div></div>
+          <div className={styles.iconsLine}>
+            <img
+              src={getImageUrl("icons/skills/react.png")}
+              className={styles.icon}
+            />{" "}
+            <img
+              src={getImageUrl("icons/skills/thatopenengine.png")}
+              className={styles.icon}
+            />{" "}
+            <img
+              src={getImageUrl("icons/skills/three.png")}
+              className={styles.icon}
+            />
+            <img
+              src={getImageUrl("icons/skills/typescript.png")}
+              className={styles.icon}
+            />{" "}
+            <img
+              src={getImageUrl("icons/skills/html.png")}
+              className={styles.icon}
+            />{" "}
+          </div>
+          <div className={styles.buttonsLine}>
+            <div className={styles.button}>
+              <p className={styles.smallText}>GitHub Repo</p>
+            </div>
+            <div className={styles.button}>
+              <p className={styles.smallText}>LinkedIn Demo</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.smallProject}>
+          <p className={styles.smallText}>
+            Master BIM Software Developer's Challenge
+          </p>
+          <p className={styles.mainText}>
+            Web-based Application to Manage BIM Projects
+          </p>
+          <p className={styles.smallText}>
+            Developed a comprehensive web application to manage projects and
+            teams using React components and a Firebase database. Integrated an
+            IFC viewer using the That Open Engine library to interact with
+            building models and project data in realtime.
+          </p>
+          <img
+            src={getImageUrl("icons/about/bimmodel.png")}
+            className={styles.projectImage}
+          />
+          <div className={styles.iconsLine}>
+            <img
+              src={getImageUrl("icons/skills/react.png")}
+              className={styles.icon}
+            />{" "}
+            <img
+              src={getImageUrl("icons/skills/thatopenengine.png")}
+              className={styles.icon}
+            />{" "}
+            <img
+              src={getImageUrl("icons/skills/three.png")}
+              className={styles.icon}
+            />
+            <img
+              src={getImageUrl("icons/skills/typescript.png")}
+              className={styles.icon}
+            />{" "}
+            <img
+              src={getImageUrl("icons/skills/html.png")}
+              className={styles.icon}
+            />{" "}
+          </div>
+          <div className={styles.buttonsLine}>
+            <div className={styles.button}>
+              <p className={styles.smallText}>GitHub Repo</p>
+            </div>
+            <div className={styles.button}>
+              <p className={styles.smallText}>LinkedIn Demo</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
