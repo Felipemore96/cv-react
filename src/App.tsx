108a25aae7b37a130e718074856f72ecbda037cc
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-24 16:56:47 -0600

    Add contact section and nav in the footer section

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => {
       <Experience />
       <Projects />
       <Contact />
+      <Nav />
     </div>
   );
 };
--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,6 +1,37 @@
-export function Contact() {
+import styles from "../styles/Contact.module.css";
+import { getImageUrl } from "../utils.ts";
 
-    return (
-        <></>
-    )
+export function Contact() {
+  return (
+    <footer className={styles.container} id="contact">
+      <p className={styles.initialText}>Get In Touch</p>
+      <h1 className={styles.title}>Contact Me</h1>
+      <div className={styles.buttonsLine}>
+        <a className={styles.button} href="mailto:felipe.moreiras796@gmail.com">
+          <img
+            src={getImageUrl("icons/main/mail.png")}
+            className={styles.icon}
+            alt="LinkeIn Profile"
+          />
+        </a>
+        <a
+          className={styles.button}
+          href="https://www.linkedin.com/in/felipemoreiras796/"
+        >
+          <img
+            src={getImageUrl("icons/main/linkedin.png")}
+            className={styles.icon}
+            alt="LinkeIn Profile"
+          />
+        </a>
+        <a className={styles.button} href="https://github.com/Felipemore96">
+          <img
+            src={getImageUrl("icons/main/github.png")}
+            className={styles.icon}
+            alt="LinkeIn Profile"
+          />
+        </a>
+      </div>
+    </footer>
+  );
 }
