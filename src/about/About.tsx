f272d861581d420dd70b347b9fc1412856675f55
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-10-25 20:40:00 -0600

    Added language icons

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -120,12 +120,12 @@ export function About() {
         </div>
         <div className={styles.mastersList}>
           <div className={styles.master}>
-            <h2 className={styles.masterText}>BIM Skills</h2>
-            <p className={styles.uniText}>{renderSkills(sortedSkills.bim)}</p>
-          </div>
-          <div className={styles.master}>
             <h2 className={styles.masterText}>Programming Skills</h2>
             <p className={styles.uniText}>{renderSkills(sortedSkills.prog)}</p>
+          </div>
+          <div className={styles.master}>
+            <h2 className={styles.masterText}>BIM Skills</h2>
+            <p className={styles.uniText}>{renderSkills(sortedSkills.bim)}</p>
           </div>
           <div className={styles.master}>
             <h2 className={styles.masterText}>Languages</h2>
